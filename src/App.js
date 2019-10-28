8f7e93db2d2d07695aee49fe58af7d60a49c7a00
vladislav1133 <vladislav1133light@ukr.net>
2019-10-28 17:50:12 +0200

    fill markup for sign in & sign up components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
-import ProjectDetails from "./components/projects/ProjectDetails";
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
               <Navbar/>
               <Switch>
                   <Route exact path='/' component={Dashboard}/>
-                  <Route path='/project/:id' component={ProjectDetails} />
+                  <Route path='/project/:id' component={ProjectDetails}/>
+                  <Route path='/signin' component={SignIn}/>
+                  <Route path='/signup' component={SignUp}/>
               </Switch>
           </div>
       </BrowserRouter>
