755e6cd9a66cdb1a8c4321bd1314db59de3e9fb2
vladislav1133 <vladislav1133light@ukr.net>
2019-10-28 18:02:11 +0200

    sign up component: fix form fields

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -31,11 +31,11 @@ class SignUp extends Component {
                     </div>
                     <div className="input-field">
                         <label htmlFor="firstName">First Name</label>
-                        <input type="firstName" id="firstName" onChange={this.handleChange}/>
+                        <input type="text" id="firstName" onChange={this.handleChange}/>
                     </div>
                     <div className="input-field">
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="lastName" id="lastName" onChange={this.handleChange}/>
+                        <input type="text" id="lastName" onChange={this.handleChange}/>
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">Login</button>
