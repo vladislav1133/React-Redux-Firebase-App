71c9edf17b5dd1a3383f594a440195846ce5d4f1
vladislav1133 <vladislav1133light@ukr.net>
2019-10-29 13:03:16 +0200

    add store project state dump data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        { id: '1', title: 'Some title - 1', content: 'Lorem ipsum dolor sit amet.' },
+        { id: '1', title: 'Some title - 1', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing.' },
+        { id: '1', title: 'Some title - 1', content: 'Lorem ipsum dolor sit amet, consectetur.' },
+    ]
 };
 const projectReducer = (state = initState, action) => {
     return state;
