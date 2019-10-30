d063221b8f90ca39bf7e0a47bacc38e24f38d5aa
vladislav1133 <vladislav1133light@ukr.net>
2019-10-30 11:33:28 +0200

    setup store with firebase libs

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from "react-redux";
 import thunk from 'redux-thunk';
-import { getFirebase } from "react-redux-firebase";
-import { getFirestore } from "redux-firestore";
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './configs/fbConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirestore, getFirebase})));
+const store = createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirestore, getFirebase})),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig)
+    )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
