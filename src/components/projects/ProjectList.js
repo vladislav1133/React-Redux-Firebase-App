ef53f2b0097cb6f619ceeb5a51fe30d248a62268
vladislav1133 <vladislav1133light@ukr.net>
2019-10-30 15:29:34 +0200

    new logic of setup firebase by version 3.0, sync projects from firestore

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
 const ProjectList = ({projects}) => {
+    const summary = [];
+
+    if(projects) {
+        for (let key in projects) {
+            summary.push(<ProjectSummary project={projects[key]} key={key} />)
+        }
+    }
     return (
         <div className="project-list section">
-            { projects && projects.map(project => {
-                return (
-                    <ProjectSummary project={project} key={project.id} />
-                )
-            })}
+            {summary}
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import {  getFirebase } from 'react-redux-firebase';
+import {reduxFirestore, getFirestore, createFirestoreInstance} from 'redux-firestore';
 import fbConfig from './configs/fbConfig'
+import {ReactReduxFirebaseProvider} from "react-redux-firebase";
 
 const middleware = [
     thunk.withExtraArgument({getFirestore})
@@ -20,7 +20,20 @@ const store = createStore(rootReducer,
     )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// react-redux-firebase config
+const rrfConfig = {
+    userProfile: 'users',
+    // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+const rrfProps = {
+    firebase: fbConfig,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance // <- needed if using firestore
+};
+
+ReactDOM.render(<Provider store={store}><ReactReduxFirebaseProvider {...rrfProps} ><App/></ReactReduxFirebaseProvider></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
